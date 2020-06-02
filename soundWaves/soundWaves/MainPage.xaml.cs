72b43bffb0b281c27fd284e3d5fea6a01d060c57
Deny S. de O. Barbosa <deny.stanley.06@gmail.com>
2020-06-02 06:36:04 -0300

    AudioGraph Playing music

--- soundWaves/soundWaves/MainPage.xaml.cs
+++ soundWaves/soundWaves/MainPage.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
 
@@ -22,6 +15,11 @@ namespace soundWaves
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        AudioFileInputNode _fileInputNode;
+        AudioGraph _graph;
+        AudioDeviceOutputNode _deviceOutputNode;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -45,6 +43,98 @@ namespace soundWaves
             {
                 this.textBlock.Text = "Operação Cancelada.";
             }
+        }
+
+        private async void Play_OnClick(object sender, RoutedEventArgs e)
+        {
+            await CreateGraph();
+            await CreateDefaultDeviceOutputNode();
+            await CreateFileInputNode();
+
+            AddReverb();
+
+            ConnectNodes();
+
+            _graph.Start();
+        }
+
+        /// <summary>
+        /// Create an audio graph that can contain nodes
+        /// </summary>       
+        private async Task CreateGraph()
+        {
+            // Specify settings for graph, the AudioRenderCategory helps to optimize audio processing
+            AudioGraphSettings settings = new AudioGraphSettings(Windows.Media.Render.AudioRenderCategory.Media);
+
+            CreateAudioGraphResult result = await AudioGraph.CreateAsync(settings);
+
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                throw new Exception(result.Status.ToString());
+            }
+
+            _graph = result.Graph;
+        }
+
+        /// <summary>
+        /// Create a node to output audio data to the default audio device (e.g. soundcard)
+        /// </summary>
+        private async Task CreateDefaultDeviceOutputNode()
+        {
+            CreateAudioDeviceOutputNodeResult result = await _graph.CreateDeviceOutputNodeAsync();
+
+            if (result.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                throw new Exception(result.Status.ToString());
+            }
+
+            _deviceOutputNode = result.DeviceOutputNode;
+        }
+
+        /// <summary>
+        /// Ask user to pick a file and use the chosen file to create an AudioFileInputNode
+        /// </summary>
+        private async Task CreateFileInputNode()
+        {
+            FileOpenPicker filePicker = new FileOpenPicker
+            {
+                SuggestedStartLocation = PickerLocationId.MusicLibrary,
+                FileTypeFilter = { ".mp3", ".wav" }
+            };
+
+            StorageFile file = await filePicker.PickSingleFileAsync();
+
+            // file null check code omitted
+
+            CreateAudioFileInputNodeResult result = await _graph.CreateFileInputNodeAsync(file);
+
+            if (result.Status != AudioFileNodeCreationStatus.Success)
+            {
+                throw new Exception(result.Status.ToString());
+            }
+
+            _fileInputNode = result.FileInputNode;
+        }
+
+        /// <summary>
+        /// Create an instance of the pre-supplied reverb effect and add it to the output node
+        /// </summary>
+        private void AddReverb()
+        {
+            ReverbEffectDefinition reverbEffect = new ReverbEffectDefinition(_graph)
+            {
+                DecayTime = 1
+            };
+
+            _deviceOutputNode.EffectDefinitions.Add(reverbEffect);
+        }
+
+        /// <summary>
+        /// Connect all the nodes together to form the graph, in this case we only have 2 nodes
+        /// </summary>
+        private void ConnectNodes()
+        {
+            _fileInputNode.AddOutgoingConnection(_deviceOutputNode);
         }
     }
 }
